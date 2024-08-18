d5ff3b7a214b6410f38c61ecd2b464e25459dbcb
alaynajohnston12 <alaynajohnston12@gmail.com>
2024-08-18 14:46:00 -0400

    fixing typo, add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { ResourcesComponent } from './resources/resources.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'resources', component: ResourcesComponent }
 ];
