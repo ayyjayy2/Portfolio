cebc7a12a6a5427fcbf7b756336b175fca53075e
alaynajohnston12 <alaynajohnston12@gmail.com>
2024-08-26 17:56:26 -0400

    remove hashing in url, begin development of portfolio version 2

---
+++ src/app/version2/version2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-version2',
+  templateUrl: './version2.component.html',
+  styleUrls: ['./version2.component.css']
+})
+export class Version2Component {
+
+}
